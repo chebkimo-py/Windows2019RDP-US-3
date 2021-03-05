7f0a56dd52b8b9aa985cf75cc7f0e27757b1296c
kurnwaj1 <80078410+kurnwaj1@users.noreply.github.com>
2021-03-05 08:45:18 +0700

    Update NGROK-CHECK.bat

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -7,7 +7,7 @@ net user administrator /active:yes >nul
 echo All done! Connect your VM using RDP.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo NGROK Services is not Running
-echo User: ajwann
+echo User: administrator
 echo Pass: ajwann
 
 
